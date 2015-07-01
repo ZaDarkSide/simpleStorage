2e5bdf9d6e68c88c7d906660f8b0f2abe845ff86
Rafee Memon <rafeememon@gmail.com>
2015-06-30 20:56:40 -0700

    Add package.json for npm support.

--- simpleStorage.js
+++ simpleStorage.js
@@ -8,6 +8,8 @@
 
     if (typeof define === 'function' && define.amd) {
         define(factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory();
     } else {
         root.simpleStorage = factory();
     }
